13f77d5f796791992f8f26bb13e803122688835d
Preetham Viswanathan <preethamvishy10@gmail.com>
2018-03-01 00:33:47 +0530

    Electron loadUrl set for dev and prod

--- main.js
+++ main.js
@@ -1,27 +1,31 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 const url = require('url')
+require('dotenv').config();
 
 let win = null;
 
 app.on('ready', function () {
 
-    // Initialize the window to our specified dimensions
     win = new BrowserWindow({ width: 600, height: 600 });
+   
+    if (process.env.PACKAGE === 'true') {
+        win.loadURL(url.format({
+            pathname: path.join(__dirname, 'dist/index.html'),
+            protocol: 'file:',
+            slashes: true
+        }));
+    } else {
+        win.loadURL(process.env.HOST);
+        win.webContents.openDevTools();
+    }
 
-    // Specify entry point
-    //   win.loadURL('http://localhost:5555');
-    win.loadURL(url.format({
-        pathname: path.join(__dirname, 'dist/index.html'),
-        protocol: 'file:',
-        slashes: true
-    }))
+
+    
 
     // Show dev tools
     // Remove this line before distributing
     win.webContents.openDevTools()
-
-    // Remove window once app is closed
     win.on('closed', function () {
         win = null;
     });

7640c9589476cf7dc207c90c23fee0bb76ff4d04
Preetham Viswanathan <preethamvishy10@gmail.com>
2018-03-01 00:07:03 +0530

    Electron base work

--- main.js
+++ main.js
@@ -1,34 +1,41 @@
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const url = require('url')
 
 let win = null;
 
 app.on('ready', function () {
 
-  // Initialize the window to our specified dimensions
-  win = new BrowserWindow({width: flex, height: flex});
+    // Initialize the window to our specified dimensions
+    win = new BrowserWindow({ width: 600, height: 600 });
 
-  // Specify entry point
-  win.loadURL('http://localhost:5555');
+    // Specify entry point
+    //   win.loadURL('http://localhost:5555');
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'dist/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
 
-  // Show dev tools
-  // Remove this line before distributing
-  win.webContents.openDevTools()
+    // Show dev tools
+    // Remove this line before distributing
+    win.webContents.openDevTools()
 
-  // Remove window once app is closed
-  win.on('closed', function () {
-    win = null;
-  });
+    // Remove window once app is closed
+    win.on('closed', function () {
+        win = null;
+    });
 
 });
 
 app.on('activate', () => {
-  if (win === null) {
-    createWindow()
-  }
+    if (win === null) {
+        createWindow()
+    }
 })
 
 app.on('window-all-closed', function () {
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
 });
